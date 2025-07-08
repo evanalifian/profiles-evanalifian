1aacf242e39491155bafcc07b790bdd5cdea04bc
evanalifian <evanalifian@gmal.com>
2025-07-08 23:12:41 +0700

    set HTML to template literals

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import data from "../../data.json";
 
 export default function Footer() {
   const year = new Date().getFullYear();
-  return html`
+  return /* HTML */`
     <footer class="mt-auto p-4">
       <p class="text-center text-sm text-gray-500">
         Copyright &copy; ${year} ${data.alias}
