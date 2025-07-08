c8a10ec46884cb3e85fad3564099b897ed54deb9
evanalifian <evanalifian@gmal.com>
2025-07-08 20:55:16 +0700

    implement prettier plugin tailwindcss on template literals

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,12 @@
-import data from "../../data.json"
+import data from "../../data.json";
 
 export default function Footer() {
-  const year = new Date().getFullYear()
-  return `
+  const year = new Date().getFullYear();
+  return html`
     <footer class="mt-auto p-4">
-      <p class="text-gray-500 text-sm text-center">Copyright &copy; ${year} ${data.alias}</p>
+      <p class="text-center text-sm text-gray-500">
+        Copyright &copy; ${year} ${data.alias}
+      </p>
     </footer>
-  `
+  `;
 }
--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,19 @@
 import data from "../../data.json";
 
 export default function Main() {
-  return `
+  return html`
     <main>
       <header class="relative">
-        <img src="/evan2.jpg" alt="Evan Alifian' photo" title="Evan Alifian" class="size-full">
-        <div class="absolute bottom-0 w-full bg-gradient-to-t from-white from-40% h-60 flex flex-col justify-end text-center p-4">
-          <span class="font-bold text-xl">${data.alias}</span>
+        <img
+          src="/evan2.jpg"
+          alt="Evan Alifian' photo"
+          title="Evan Alifian"
+          class="size-full"
+        />
+        <div
+          class="absolute bottom-0 flex h-60 w-full flex-col justify-end bg-gradient-to-t from-white from-40% p-4 text-center"
+        >
+          <span class="text-xl font-bold">${data.alias}</span>
           <p class="text-gray-700">Hi! i'm ${data.name}.</p>
         </div>
       </header>
@@ -17,7 +24,7 @@ export default function Main() {
           <a href="${social.url}" target="_blank" class="text-gray-700 transition-colors hover:text-black">
             ${social.icon}
           </a>
-        `
+        `,
           )
           .join("")}
       </section>
